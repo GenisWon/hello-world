c9f4f5a8e60b705f1375ad5c91507c9f6305bed4
GenisWon <bbgenis11@gmail.com>
2015-01-15 20:45:27 -0500

    first write, works

---
+++ a1/a1.cpp
@@ -0,0 +1,15 @@
+// caedmon delvecchio
+// a1.cpp
+// thursday
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+	cout << "hello world" << endl;
+	system("pause");
+
+	return 0;
+}//end of main
